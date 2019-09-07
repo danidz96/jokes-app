8fea312fec5fd4a6d9b8dd54b74361cea58b0e89
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-07 13:35:24 +0200

    add dynamic color and emojis

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -2,16 +2,54 @@ import React from 'react';
 import './Joke.css';
 
 const Joke = (props) => {
+	const getColor = () => {
+		if (props.votes >= 15) {
+			return '#4CAF50';
+		} else if (props.votes >= 12) {
+			return '#8BC34A';
+		} else if (props.votes >= 9) {
+			return '#CDDC39';
+		} else if (props.votes >= 6) {
+			return '#FFEB3B';
+		} else if (props.votes >= 3) {
+			return '#FFC107';
+		} else if (props.votes >= 0) {
+			return '#FF9800';
+		} else {
+			return '#f44336';
+		}
+	};
+
+	const getEmoji = () => {
+		if (props.votes >= 15) {
+			return 'em em-rolling_on_the_floor_laughing';
+		} else if (props.votes >= 12) {
+			return 'em em-laughing';
+		} else if (props.votes >= 9) {
+			return 'em em-smiley';
+		} else if (props.votes >= 6) {
+			return 'em em-slightly_smiling_face';
+		} else if (props.votes >= 3) {
+			return 'em em-neutral_face';
+		} else if (props.votes >= 0) {
+			return 'em em-confused';
+		} else {
+			return 'em em-angry';
+		}
+	};
+
 	return (
 		<div className="joke">
 			<div className="joke-buttons">
 				<i className="fas fa-arrow-up" onClick={props.upvote} />
-				<span className="joke-votes">{props.votes}</span>
+				<span className="joke-votes" style={{ borderColor: getColor() }}>
+					{props.votes}
+				</span>
 				<i className="fas fa-arrow-down" onClick={props.downvote} />
 			</div>
 			<div className="joke-text">{props.text}</div>
 			<div className="joke-smiley">
-				<i className="em em-rolling_on_the_floor_laughing" />
+				<i className={getEmoji()} />
 			</div>
 		</div>
 	);
