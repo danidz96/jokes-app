794a4f4f111e458b70f9042d0d3f1e6107ee4a81
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-09-07 13:24:34 +0200

    style joke component

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './Joke.css';
 
 const Joke = (props) => {
 	return (
 		<div className="joke">
 			<div className="joke-buttons">
 				<i className="fas fa-arrow-up" onClick={props.upvote} />
-				<span>{props.votes}</span>
+				<span className="joke-votes">{props.votes}</span>
 				<i className="fas fa-arrow-down" onClick={props.downvote} />
 			</div>
 			<div className="joke-text">{props.text}</div>
+			<div className="joke-smiley">
+				<i className="em em-rolling_on_the_floor_laughing" />
+			</div>
 		</div>
 	);
 };
